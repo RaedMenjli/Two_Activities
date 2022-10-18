ed2117b0020187fa318f879446bba62ed946eeeb
POST6 <post6.prog2@enetcom.usf.tn>
2022-10-18 17:30:14 +0200

    initial commit

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.twoactivities.R;
+import com.example.twoactivities.SecondActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+}
