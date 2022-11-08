4b0198f3657533305287d510e328d7473e8fd2c4
POST6 <eya.ibn.hamda@gmail.com>
2022-11-08 15:45:55 +0100

    tp2.2

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 
 public class SecondActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     private EditText mReply;
     public static final String EXTRA_REPLY= "com.example.android.twoactivities.extra.REPLY";
 
@@ -26,13 +26,48 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
         Log.d("reply0", reply);
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
